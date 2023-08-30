448d425c2742b47095139c792954810debbbd062
Tea-Key1 <ingethu2@gmail.com>
2023-08-30 10:40:26 +0900

    一応完成形に近づきました。あとは、プロセスのデザインと全体に加算するデザイン

--- src/Components/Interface.jsx
+++ src/Components/Interface.jsx
@@ -178,7 +178,7 @@ function Process() {
   return (
     <section className="h-[100dvh] w-screen px-8 max-w-screen-2xl max-auto flex md:flex-row-reverse flex-col relative align-middle border-2 border-teal-500">
       <div className="h-full w-full flex flex-col justify-center">
-        <div className="bg-white h-auto w-full flex flex-col justify-center rounded-2xl p-5 sm:ml-5 mb-10">
+        <div className="bg-white h-auto w-full flex flex-col justify-center rounded-2xl p-5 sm:ml-5">
           <div className="flex relative pb-12">
             <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
               <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
@@ -215,7 +215,7 @@ function Process() {
               <p className="text-xs">ご提示させていただいた金額と内容にご納得いただけましたら、お申込み契約の手続きを進め、制作をスタートさせます。プロジェクトの規模によりますが、ひと月から半年程度の期間がかかることがあります。</p>
             </div>
           </div>
-          <div className="flex relative pb-12">
+          <div className="flex relative pb-5">
             <div className="flex-shrink-0 w-9 h-9 rounded-full bg-pink-100 inline-flex items-center justify-center text-white relative z-10">
               <img src="/textures/step04.svg" />
             </div>
@@ -232,9 +232,9 @@ function Process() {
             <figure className="h-auto ml-auto mx-10 relative z-[1] rounded-b-lg shadow-[0_2.75rem_3.5rem_-2rem_rgb(45_55_75_/_20%),_0_0_5rem_-2rem_rgb(45_55_75_/_15%)] dark:shadow-[0_2.75rem_3.5rem_-2rem_rgb(0_0_0_/_20%),_0_0_5rem_-2rem_rgb(0_0_0_/_15%)]">
               <div className="relative flex items-center max-w-[50rem] bg-gray-800 rounded-t-lg py-2 px-24">
                 <div className="flex space-x-1 absolute top-2/4 left-4 -translate-y-1">
-                  <span className="w-2 h-2 bg-red-600 rounded-full"/>
-                  <span className="w-2 h-2 bg-yellow-600 rounded-full"/>
-                  <span className="w-2 h-2 bg-green-600 rounded-full"/>
+                  <span className="w-2 h-2 bg-red-600 rounded-full" />
+                  <span className="w-2 h-2 bg-yellow-600 rounded-full" />
+                  <span className="w-2 h-2 bg-green-600 rounded-full" />
                 </div>
                 <div className="flex justify-center items-center w-full h-full bg-gray-700 text-[.25rem] text-gray-400 rounded-sm sm:text-[.5rem] dark:bg-gray-600 dark:text-gray-400">www.askone.com</div>
               </div>
@@ -266,43 +266,23 @@ const Cost = () => {
               <h1 className="mt-4 text-sm">表示価格は基準料金です。内容・規模により正確な料金をお見積りいたします。</h1>
 
               <div className="mt-6 space-y-8 xl:mt-12">
-                <div className="flex items-center justify-between max-w-2xl px-8 py-4 mx-auto border cursor-pointer rounded-xl dark:border-gray-700">
-                  <div className="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-gray-400 sm:h-9 sm:w-9" viewBox="0 0 20 20" fill="currentColor">
-                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                    </svg>
-
-                    <div className="flex flex-col items-center mx-5 space-y-1">
-                      <h2 className="text-lg font-medium text-gray-700 sm:text-2xl dark:text-gray-200">Basic</h2>
-
-                      <div className="px-2 text-xs text-blue-500 bg-gray-100 rounded-full sm:px-4 sm:py-1 dark:bg-gray-700 ">
-                        Save 20%
-                      </div>
-                    </div>
+                <div className="flex items-center justify-between max-w-2xl px-8 py-4 mx-auto border border-amber-800 rounded-xl ">
+                  <div className="flex flex-col items-center justify-center">
+                    <h2 className="text-sm whitespace-nowrap">月額プラン</h2>
                   </div>
-
-                  <h2 className="text-2xl font-semibold text-gray-500 sm:text-3xl dark:text-gray-300">$49 <span className="text-base font-medium">/Month</span></h2>
+                  <div className="flex flex-col items-center justify-center">
+                    <h2 className=" text-lg font-semibold">3万円~</h2><span className="text-base font-medium">/月</span>
+                  </div>
                 </div>
 
-                <div className="flex items-center justify-between max-w-2xl px-8 py-4 mx-auto border border-blue-500 cursor-pointer rounded-xl">
-                  <div className="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-blue-600 sm:h-9 sm:w-9" viewBox="0 0 20 20" fill="currentColor">
-                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                    </svg>
-
-                    <div className="flex flex-col items-center mx-5 space-y-1">
-                      <h2 className="text-lg font-medium text-gray-700 sm:text-2xl dark:text-gray-200">Popular</h2>
-                      <div className="px-2 text-xs text-blue-500 bg-gray-100 rounded-full sm:px-4 sm:py-1 dark:bg-gray-700 ">
-                        Save 20%
-                      </div>
-                    </div>
+                <div className="flex items-center justify-between max-w-2xl px-8 py-4 mx-auto border border-amber-800 rounded-xl">
+                  <div className="flex flex-col items-center justify-center">
+                    <h2 className="text-sm whitespace-nowrap">初期費用</h2><p>+</p><h2 className="text-sm whitespace-nowrap">定額プラン</h2>
                   </div>
-
-                  <h2 className="text-2xl font-semibold text-blue-600 sm:text-4xl">$99 <span className="text-base font-medium">/Month</span></h2>
+                  <div className="flex flex-col items-center justify-center">
+                    <h2 className=" text-lg font-semibold">30万+2万~</h2><span className="text-base font-medium">/月</span>
+                  </div>
                 </div>
-
-
-
 
               </div>
             </div>
@@ -439,7 +419,7 @@ const About = () => {
 
       <div className="flex flex-col align-middle items-center justify-center">
 
-        <div className="max-w-2xl shadow shadow-gray-400 sm:rounded-lg bg-white">
+        <div className="max-w-2xl shadow shadow-gray-400 sm:rounded-lg bg-white mt-10">
           <div className="px-4 py-5 sm:px-6">
             <h3 className=" text-center">
               組織概要
@@ -499,9 +479,9 @@ const About = () => {
             <a href="/question" className="hover:translate-y-[-5px] transition duration-500">よくある質問FAQ</a>
             <a href="/privacy" className="hover:translate-y-[-5px] transition duration-500">個人情報保護に関する方針</a>
           </nav>
-          <div className="text-gray-600 body-font">
+          <div className="body-font">
             <div className="container px-2 py-2 xs:py-3 mx-auto flex items-center justify-center">
-              <a className="flex title-font font-medium items-center text-gray-900">
+              <a className="flex title-font font-medium items-center">
                 <img src="\textures\astronauts.svg" className="w-12 h-12 p-1 rounded-full" />
                 {width >= 11 ? (
                   <p className="ml-2 text-2lg">
